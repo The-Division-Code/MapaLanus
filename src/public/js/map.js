01e02f3bcf20c9ab868d1972f9b67698c5b448ee
nicocaputocai <nicolas_caputo@hotmail.com>
2021-09-16 13:24:37 -0300

    colocados iconos de salud y seguridad

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -5,9 +5,9 @@ window.addEventListener('load',function(){
 function onEachFeature(feature, layer) {
     
     // does this feature have a property named popupContent?
-  if (feature.properties) {
-        layer.bindPopup(`<h3>${feature.properties.name}</h3><span>Dirección: ${feature.properties.address} ${feature.properties.height}</span>`);
-  }
+  // if (feature.properties) {
+      return  layer.bindPopup(`<h3>${feature.properties.name}</h3><span>Dirección: ${feature.properties.address} ${feature.properties.height}</span>`);
+  // }
 }
 // var quilmes = L.marker([-34.730302,  -58.268868]).bindPopup('This is Quilmes, CO.'),
 //     lomas    = L.marker([-34.7572582, -58.4026638]).bindPopup('This is Lomas, CO.'),
@@ -32,13 +32,39 @@ async function setMap() {
     center: [-34.7033363,-58.3953235], 
     zoom: 13, 
   })
-  //   var healthIcon = L.icon({
-  //     iconUrl: 'health.png',
-  //     iconSize: [38, 95],
-  //     iconAnchor: [22, 94],
-  //     shadowSize: [68, 95],
-  //     shadowAnchor: [22, 94]
-  // });
+
+    var healthIconProvincial =new L.icon({
+      iconUrl: '/img/health.svg',
+      iconSize: [80, 40],
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94]
+  });
+  var healthIconMunicipal =new L.icon({
+    iconUrl: '/img/health2.svg',
+    iconSize: [80, 40],
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+var healthIconPrivate =new L.icon({
+  iconUrl: '/img/healthPrivate.svg',
+  iconSize: [80, 40],
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94]
+});
+
+var policeIcon =new L.icon({
+  iconUrl: '/img/police.svg',
+  iconSize: [80, 30],
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94]
+});
+
+var bombersIcon =new L.icon({
+  iconUrl: '/img/bombers.svg',
+  iconSize: [80, 40],
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94]
+});
 
     
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
@@ -86,14 +112,37 @@ async function setMap() {
       onEachFeature:onEachFeature
     }).addTo(map);
 
+    // var health= L.geoJSON(healthData, {
+    //   data: healthData,
+    //   onEachFeature: onEachFeature,
+    //   icon: healthIcon
+    // }).addTo(map);
     var health= L.geoJSON(healthData, {
-      data: healthData,
       onEachFeature: onEachFeature,
+      pointToLayer: function(feature,latlng){
+        if(feature.properties.dependence == "Municipal"){
+          return L.marker(latlng,{icon: healthIconMunicipal});
+        }else if(feature.properties.dependence == "Provincial"){
+          return L.marker(latlng,{icon: healthIconProvincial});
+        }else{
+          return L.marker(latlng,{icon: healthIconPrivate});
+        }
+
+      }
     }).addTo(map);
     
     var security = L.geoJSON(securityData,{
-      data: securityData,
-      onEachFeature: onEachFeature
+      onEachFeature: onEachFeature,
+      pointToLayer: function(feature,latlng){
+        if(feature.properties.tipo == "Comisaría" || feature.properties.tipo == "Comisaria"){
+          return L.marker(latlng,{icon: policeIcon});
+        }else if(feature.properties.tipo == "Cuartel de Bomberos"){
+          return L.marker(latlng,{icon: bombersIcon});
+        // }else{
+        //   return L.marker(latlng,{icon: healthIconPrivate});
+        }
+
+      }
     }).addTo(map);
 
     var transport = L.geoJSON(transportData,{
