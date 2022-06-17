7126765bdb08b60b5b08257e013dd0a40d210016
nicocaputocai <nicolas_caputo@hotmail.com>
2022-06-17 13:01:42 -0300

    sacando merges

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -222,7 +222,7 @@ async function setMap() {
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
     {
       attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Division Code & Desarrollo i, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 19,
       minZoom: 12,
       id: "mapbox/streets-v11",
