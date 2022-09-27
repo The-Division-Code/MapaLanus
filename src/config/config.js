844d0812529a6ffe45d8803cef99360db00a799b
The Division Code <114432890+The-Division-Code@users.noreply.github.com>
2022-09-27 10:44:53 -0300

    Delete config.js

--- src/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3030,
-    DB: process.env.DB || 'mongodb+srv://desarrolloi:TJDz2pCs6qnNLL10@cluster0.wug6n.mongodb.net/mapaLanus?retryWrites=true&w=majority'
-}
